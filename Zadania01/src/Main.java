962b6b6b31e19fae8c9b3cf0bc58fa0fabfcf96f
Michał Straszak <test@test.com>
2026-09-11 12:01:53 +0200

    komentarz

--- Zadania01/src/Main.java
+++ Zadania01/src/Main.java
@@ -1,7 +1,16 @@
 void main() {
 
-            // Zad.1 Wyświetlenie trzech imion w trzech kolejnych liniach
-            System.out.println("Ania");
-            System.out.println("Bartek");
-            System.out.println("Kasia");
+    // Zad.1 Wyświetlenie trzech imion w trzech kolejnych liniach
+    System.out.println("Ania");
+    System.out.println("Bartek");
+    System.out.println("Kasia");
+
+    // Zad.2
+
+    String imie = "Michał";
+    int rok = 2007;
+    double liczba = 0.66;
 }
+
+
+    
