4119013597cff3a09e47816e1b914f2ba45922c7
Michał Straszak <test@test.com>
2026-09-11 12:12:21 +0200

    komentarze

--- Zadania01/src/Main.java
+++ Zadania01/src/Main.java
@@ -10,7 +10,12 @@ void main() {
     String imie = "Michał";
     int rok = 2007;
     double liczba = 0.66;
+
+
+    // Zad.3
+    int wiek = 2026 - rok;
+    System.out.println("Mam na imię " + imie + ", mam " + wiek + " lat i będę pisać maturę za " + liczba + " roku.");
 }
 
 
-    
+
