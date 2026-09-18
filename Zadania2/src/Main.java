2258272e8443a5c222941b37f199d08de52d15cd
Michał Straszak <email>
2026-09-18 12:24:41 +0200

    kom

--- Zadania2/src/Main.java
+++ Zadania2/src/Main.java
@@ -199,3 +199,23 @@ public class Main {
 
 
 // Zad 8
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj rok: ");
+        int rok = scanner.nextInt();
+
+        if ((rok % 4 == 0 && rok % 100 != 0) || rok % 400 == 0) {
+            System.out.println("Podany rok jest rokiem przestępnym.");
+        } else {
+            System.out.println("Podany rok nie jest rokiem przestępnym.");
+        }
+
+        scanner.close();
+    }
+}
