7fc098ca2c7a55d787ea2c50420e14759ab7e29a
Michał Straszak <email>
2026-09-18 11:53:40 +0200

    kom

--- Zadania2/src/Main.java
+++ Zadania2/src/Main.java
@@ -70,3 +70,42 @@ public class Main {
         }
     }
 }
+
+
+// Zad 4
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj pierwszą liczbę: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Podaj drugą liczbę: ");
+        int b = scanner.nextInt();
+
+        System.out.print("Podaj trzecią liczbę: ");
+        int c = scanner.nextInt();
+
+        int najwieksza = a;
+
+        if (b > najwieksza) {
+            najwieksza = b;
+        }
+
+        if (c > najwieksza) {
+            najwieksza = c;
+        }
+
+        System.out.println("Największa liczba to: " + najwieksza);
+    }
+}
+
+
+// Zad 5
+
+
+
+
